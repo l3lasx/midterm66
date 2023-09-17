78ba58645e5f884a8520c866f735e02f4450031b
l3lasx <65011212023@msu.ac.th>
2023-09-18 03:35:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -105,6 +105,12 @@ if (isset($genres[$type])) {
         ?>
     </div>
 
+    <div class="row" style="margin-top: 40px;">
+        <div class="col-12 text-light" style="display: flex; justify-content: center;">
+            Copy โดย บาส CSMSU @ 2023
+        </div>
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
